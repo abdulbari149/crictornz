ada67ed48e2f21575e3672b3395e44cd116da30c
abdulbari149 <abdulbari131103@gmail.com>
2022-10-15 22:26:07 +0500

    Project Setup: Crictornz

---
+++ src/main/java/com/abdulbari149/crictornz/CrictornzApplication.java
@@ -0,0 +1,13 @@
+package com.abdulbari149.crictornz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrictornzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrictornzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdulbari149/crictornz/CrictornzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulbari149.crictornz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrictornzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
